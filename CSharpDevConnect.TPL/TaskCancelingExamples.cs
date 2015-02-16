8f95a0e32667406114fb937334ef676bd54a3803
mparkhill <mike.parkhill@magnetforensics.com>
2015-02-16 11:43:49 -0500

    Added taak canceling examples

---
+++ CSharpDevConnect.TPL/TaskCancelingExamples.cs
@@ -0,0 +1,193 @@
+﻿
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace CSharpDevConnect.TPL.Examples
+{
+    public sealed class TaskCancelingExamples
+    {
+        [Fact]
+        public void GracefulCancelExample()
+        {
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            TaskIterationCanceller iterationCanceller = new TaskIterationCanceller(5, tokenSource);
+
+            Task taskToCancel = new Task(() =>
+                {
+                    while (!tokenSource.IsCancellationRequested)
+                    {
+                        iterationCanceller.CheckForCancelIteration();
+                        Thread.Sleep(100);
+                    }
+                });
+
+            taskToCancel.Start();
+            taskToCancel.Wait();
+
+            // Final status will be RanToCompletion
+            ConsoleLogger.Log(string.Format("Task final state: {0}", taskToCancel.Status));
+        }
+
+        [Fact]
+        public void ControlledCancelExceptionExample()
+        {
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            TaskIterationCanceller iterationCanceller = new TaskIterationCanceller(5, tokenSource);
+
+            Task taskToCancel = new Task(() =>
+            {
+                while (true)
+                {
+                    if (tokenSource.IsCancellationRequested)
+                    {
+                        // Do any needed clean up here.
+
+                        tokenSource.Token.ThrowIfCancellationRequested();
+                    }
+
+                    iterationCanceller.CheckForCancelIteration();
+                    Thread.Sleep(100);
+                }
+            }, tokenSource.Token);
+
+            taskToCancel.Start();
+
+            try
+            {
+                taskToCancel.Wait();
+            }
+            catch (AggregateException ae)
+            {
+                foreach (Exception innerException in ae.InnerExceptions)
+                {
+                    if (innerException is TaskCanceledException)
+                    {
+                        ConsoleLogger.Log("Task Was Cancelled.");
+                    }
+                    else
+                    {
+                        throw innerException;
+                    }
+                }
+            }
+
+            // Final status will be Canceled
+            ConsoleLogger.Log(string.Format("Task final state: {0}", taskToCancel.Status));
+        }
+
+        [Fact]
+        public void HardCancelInWaitExample()
+        {
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            TaskIterationCanceller iterationCanceller = new TaskIterationCanceller(5, tokenSource);
+
+            Task taskToCancel = new Task(() =>
+            {
+                while (true)
+                {
+                    iterationCanceller.CheckForCancelIteration();
+                    Thread.Sleep(100);
+                }
+            });
+
+            taskToCancel.Start();
+
+            try
+            {
+                taskToCancel.Wait(tokenSource.Token);
+            }
+            catch (OperationCanceledException e)
+            {
+                ConsoleLogger.Log(string.Format("Caught OperationCanceledException: {0}", e));
+            }
+
+            // Final status will be Running
+            ConsoleLogger.Log(string.Format("Task final state: {0}", taskToCancel.Status));
+        }
+
+        [Fact]
+        public void HardCancelInTaskConstructorExample()
+        {
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            TaskIterationCanceller iterationCanceller = new TaskIterationCanceller(5, tokenSource);
+
+            // By passing the token in the constructor, it will prevent the task from being allocated 
+            // if the token source is cancelled before it is scheduled/started
+            Task taskToCancel = new Task(() =>
+            {
+                while (true)
+                {
+                    iterationCanceller.CheckForCancelIteration();
+
+                    Thread.Sleep(100);
+                }
+            }, tokenSource.Token);
+
+            tokenSource.Cancel();
+
+            try
+            {
+                // Because the token has already been cancelled, this will throw an InvalidOperationException
+                taskToCancel.Start();
+
+                taskToCancel.Wait();
+            }
+            catch (InvalidOperationException e)
+            {
+                ConsoleLogger.Log(string.Format("Caught InvalidOperationException: {0}", e));
+            }
+
+            // Final status will be Canceled
+            ConsoleLogger.Log(string.Format("Task final state: {0}", taskToCancel.Status));
+        }
+
+
+        /// <summary>
+        /// A simple helper class that will cancel a <see cref="CancellationTokenSource"/> after
+        /// a set number of calls
+        /// </summary>
+        class TaskIterationCanceller
+        {
+            private readonly int _cancelIteration;
+
+            private int _currentIteration = 0;
+
+            private readonly CancellationTokenSource _cancellationTokenSource;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="TaskIterationCanceller"/> class.
+            /// </summary>
+            /// <param name="cancelIteration">The number of iterations to wait before calling Cancel() on <paramref name="cancellationTokenSource"/></param>
+            /// <param name="cancellationTokenSource">The <see cref="CancellationTokenSource"/> to act upon.</param>
+            public TaskIterationCanceller(int cancelIteration, CancellationTokenSource cancellationTokenSource)
+            {
+                _cancelIteration = cancelIteration;
+                _cancellationTokenSource = cancellationTokenSource;
+            }
+
+            /// <summary>
+            /// Checks to see if the token source should be cancelled.
+            /// </summary>
+            /// <returns>True, if the token source has been cancelled.</returns>
+            public bool CheckForCancelIteration()
+            {
+                _currentIteration++;
+
+                if (_currentIteration >= _cancelIteration)
+                {
+                    ConsoleLogger.Log(string.Format("Cancelling task after {0} iterations", _currentIteration));
+                    _cancellationTokenSource.Cancel();
+                }
+
+                return _cancellationTokenSource.IsCancellationRequested;
+            }
+        }
+    }
+}
--- CSharpDevConnect.TPL/TaskExceptionHandlingExamples.cs
+++ CSharpDevConnect.TPL/TaskExceptionHandlingExamples.cs
@@ -9,43 +9,6 @@ namespace CSharpDevConnect.TPL.Examples
     public class TaskExceptionHandlingExamples
     {
         [Fact]
-        public void HandlingExceptionFromTaskExceptionPropertyExample()
-        {
-            Task task1 = new Task(() =>
-            {
-                throw new Exception("Task1 faulted.");
-            });
-
-            // Add error handling child task
-            Task errorHandlerTask = task1.ContinueWith((t) =>
-                {
-                    if (t.IsFaulted && t.Exception != null)
-                    {
-                        t.Exception.Handle((e) =>
-                            {
-                                ConsoleLogger.Log(string.Format("I have handled a {0}", e.GetType().Name));
-
-                                // mark the exception as handled, 
-                                // if the exception is not handled than an AggregateException will be 
-                                // thrown when Wait() or Result is called on errorHandlerTask
-                                return true;
-                            });
-                    }
-                }, TaskContinuationOptions.OnlyOnFaulted);
-
-            try
-            {
-                task1.Start();
-
-                errorHandlerTask.Wait(500);
-            }
-            catch (AggregateException ae)
-            {
-                ConsoleLogger.Log("AggregateException caught on wait.");
-            }
-        }
-
-        [Fact]
         public void SimpleAggregateExceptionExample()
         {
             Task task = new Task(() =>
@@ -60,10 +23,10 @@ namespace CSharpDevConnect.TPL.Examples
             }
             catch (AggregateException ae)
             {
+                // Iterate over all of the inner exceptions 
                 foreach (Exception innerException in ae.InnerExceptions)
                 {
                     ConsoleLogger.Log(string.Format("Caught {0}.", innerException));
-
                 }
             }
         }
@@ -99,5 +62,43 @@ namespace CSharpDevConnect.TPL.Examples
                 }
             }
         }
+
+        [Fact]
+        public void HandlingExceptionFromTaskExceptionPropertyExample()
+        {
+            Task task1 = new Task(() =>
+            {
+                throw new Exception("Task1 faulted.");
+            });
+
+            // Add error handling child task
+            Task errorHandlerTask = task1.ContinueWith((t) =>
+            {
+                if (t.IsFaulted && t.Exception != null)
+                {
+                    t.Exception.Handle((e) =>
+                    {
+                        ConsoleLogger.Log(string.Format("I have handled a {0}", e.GetType().Name));
+
+                        // mark the exception as handled, 
+                        // if the exception is not handled than an AggregateException will be 
+                        // thrown when Wait() or Result is called on errorHandlerTask
+                        return true;
+                    });
+                }
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            try
+            {
+                task1.Start();
+
+                errorHandlerTask.Wait(500);
+            }
+            catch (AggregateException ae)
+            {
+                ConsoleLogger.Log("AggregateException caught on wait.");
+            }
+        }
+
     }
 }
